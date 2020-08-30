2e6dc6519f8074c76195e9a58334a4f1d75a5a6c
Adam White <adamfrankwhite@gmail.com>
2020-08-30 18:51:13 +0100

    features mq

--- footer.php
+++ footer.php
@@ -40,24 +40,13 @@ dynamic_sidebar('footer-sidebar-3');
 
     <hr>
     <ul class="social-media">
-        <a href="http://www.facebook.com/sharer.php?u=https://www.quittingweedthebook.com" target="_blank">
+        <a href="https://www.facebook.com/heatonscats/" target="_blank">
             <li><i class="fa fa-facebook" aria-hidden="true"></i></li>
         </a>
-        <a href="https://twitter.com/share?url=https://www.quittingweedthebook.com&amp;text=Quitting%20Weed%20The%20Complete%20Guide;hashtags=cannabis-addiction"
-            target="_blank">
+        <a href="https://twitter.com/HeatonsCats" target="_blank">
             <li><i class="fa fa-twitter" aria-hidden="true"></i></li>
         </a>
-        <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=https://www.quittingweedthebook.com"
-            target="_blank">
-            <li><i class="fa fa-linkedin" aria-hidden="true"></i></li>
-        </a>
-        <a
-            href="javascript:void((function()%7Bvar%20e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','http://assets.pinterest.com/js/pinmarklet.js?r='+Math.random()*99999999);document.body.appendChild(e)%7D)());">
-            <li><i class="fa fa-instagram" aria-hidden="true"></i></li>
-        </a>
-        <a href="http://reddit.com/submit?url=https%3A%2F%2Fwww.quittingweedthebook.com" target="_blank">
-            <li><i class="fa fa-reddit" aria-hidden="true"></i></li>
-        </a>
+
     </ul>
 </footer>
 
