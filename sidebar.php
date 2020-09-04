54ca26f9e6f553aaab18539ee30fb9a46da54c5f
Adam White <adamfrankwhite@gmail.com>
2020-09-04 15:51:13 +0100

    added sidebar cat slider

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,15 @@
 <div class="col-sm-4 col-sm-offset-1 blog-sidebar">
+    <?php 
+    if (!is_category("cats") && !is_category("kittens")) {
+        echo '<div class="cat-gallery-cont">
+                <i id="left" class="fa fa-chevron-circle-left" aria-hidden="true"></i>
+                <div id="sidebar-gallery"></div>
+                <i id="right" class="fa fa-chevron-circle-right" aria-hidden="true"></i>
+            </div>';
+        };
+        ?>
+
+
     <div class="sidebar-module sidebar-module-inset adopt-side-widget">
         <img class="icon" src="<?php bloginfo('template_url');?>/images/icons/icons8-cat-caregivers.svg" />
         <?php
@@ -20,3 +31,83 @@
         ?>
     </div>
 </div>
+
+<script>
+jQuery(document).ready(function() {
+    //Perform Ajax request.
+    jQuery.ajax({
+        url: 'http://localhost/heatons/wp-json/wp/v2/posts?_embed',
+        type: 'get',
+        success: function(data) {
+            const catPosts = data
+            console.log(catPosts)
+            const catGallery = document.getElementById('sidebar-gallery')
+            // Check categories only cats/kitten
+            let catGalleryData = catPosts.filter(post => post.categories.includes(2) || post
+                    .categories.includes(3))
+                .map(post => {
+                        return {
+
+                            name: post.title.rendered,
+                            img_url: post.better_featured_image.source_url,
+                            link: post.link
+                        }
+                    }
+
+                )
+            console.log(catGalleryData)
+
+            function getGallery(numSlides = 4) {
+                let galleryHTML = ""
+                let slideView = catGalleryData.slice(0, numSlides)
+                slideView.forEach(catPost => {
+                    galleryHTML += `
+                <a href="${catPost.link}">
+                
+                    <div class="card"> 
+                        <div class="card-content">
+                            <img src="${catPost.img_url}" alt=${catPost.name}/>
+                            <h4>${catPost.name}</h4>
+                        </div>
+                    </div>
+                </a>
+                `
+                })
+                catGallery.innerHTML = galleryHTML
+            }
+
+            function slider(direction) {
+                if (direction == "left") {
+                    let lastCat = catGalleryData.shift()
+                    catGalleryData.push(lastCat)
+                    getGallery(1)
+                } else {
+                    let lastCat = catGalleryData.pop()
+                    catGalleryData.unshift(lastCat)
+                    getGallery(1)
+                }
+
+            }
+
+            //On load
+            getGallery(1)
+
+            //Button listens
+            jQuery('#left').on('click', () => slider("left"))
+            jQuery('#right').on('click', () => slider("right"))
+
+
+
+
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            var errorMsg = 'Ajax request failed: ' + xhr.responseText;
+            $('#content').html(errorMsg);
+        }
+    });
+
+
+
+
+});
+</script>
